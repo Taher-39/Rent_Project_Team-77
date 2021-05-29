2a45754d19ffcbd5f2b77016de4a6e018412c8ee
Abu Taher <abut70243@gmail.com>
2021-05-29 15:58:16 +0600

    after basic

--- src/components/Apartment/Apartment.js
+++ src/components/Apartment/Apartment.js
@@ -2,9 +2,19 @@ import React from 'react';
 
 const Apartment = () => {
     return (
-        <div>
-            this is apartment
-        </div>
+        <section>
+            {/* Home details heading */}
+            <div>
+                
+            </div>
+            {/* Home details and booking part */}
+            <div className="container">
+                <div className='row'>
+                    <div>Details part</div>
+                    <div>Booking Part</div>
+                </div>
+            </div>
+        </section>
     );
 };
 
