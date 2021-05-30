70943694308b330f59d52fbb86c26724498ef95e
Abu Taher <abut70243@gmail.com>
2021-05-30 11:12:48 +0600

    after header part

--- src/components/Apartment/Apartment.js
+++ src/components/Apartment/Apartment.js
@@ -1,20 +1,24 @@
 import React from 'react';
-
+import './Apartment.css'
 const Apartment = () => {
     return (
-        <section>
-            {/* Home details heading */}
-            <div>
-                
-            </div>
-            {/* Home details and booking part */}
-            <div className="container">
+        <div>
+            <header className="apartment-banner">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6x2MeF5TbLN0nu6R6TWnlqmttEKN2-jifZg&usqp=CAU" width="100%" height="250vh" />
+                <h2 className="logo-overly text-light" width="100px">Apartment</h2>
+                <div className="color-overlay"></div>
+            </header>
+            <main className='container'>
                 <div className='row'>
-                    <div>Details part</div>
-                    <div>Booking Part</div>
+                    <div className='col-md-8'>
+                        
+                    </div>
+                    <div className='col-md-4'>
+
+                    </div>
                 </div>
-            </div>
-        </section>
+            </main>
+        </div>
     );
 };
 
